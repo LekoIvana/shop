d38ff2b20d47b0a2a4a80028cd04bc69b2df465c
LekoIvana <ivanaaleko@gmail.com>
2024-03-12 00:44:31 +0100

    promjene4

--- resources/js/views/SearchResult.vue
+++ resources/js/views/SearchResult.vue
@@ -41,9 +41,9 @@
             </ul>
             <div class="card-body d-flex justify-content-between" style="background-color: #E4D9D9 !important;">
                 <a href="#"  class="card-link fs-4 text-muted text-decoration-underline">{{ product.price }}</a>
-                <a  href="#" v-if="isUserLogin" @click="addCart(product.id)" class="card-link"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart cartSvg" viewBox="0 0 16 16">
-  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-</svg></a>
+                <a href="#" v-if="isLoggedIn" @click="addCart(product.id)" class="card-link"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart cartSvg" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                </svg></a>
             </div>
         </div>
     </div>
@@ -52,6 +52,10 @@
 </template>
 
 <script>
+    import { mapState } from "vuex";
+    import axios from "axios";
+    import { mapGetters } from "vuex";
+    import { mapActions } from "vuex";
     export default {
     data() {
         return {
@@ -61,6 +65,14 @@
             isUserLogin: false,
            
         };
+    },
+    computed: {
+        
+        ...mapGetters(["loggedInUser"]),
+
+        isLoggedIn() {
+            return this.loggedInUser !== null;
+        },
     },
     mounted() {
        
